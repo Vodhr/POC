af39609f5b9935ded50b5e1389e513d24f68d4be
Benjamin Altenstein <post@benni-altenstein.de>
2020-04-12 13:12:40 +0200

    Added Files for BQ76952 Battery Management System IC

---
+++ BQ76952.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "I2CDevice.h"
+class BQ76952 :
+	public I2CDevice {
+	BQ76952();
+
+	struct Status5 {
+
+	};
+
+	void init() final;
+	void selfTest() final;
+
+	Status5 getStatus5();
+};
+
